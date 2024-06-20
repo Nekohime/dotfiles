0e706b28bcdc3913974d7a975ae4d725791e2340
Nekohime <Nekohime@users.noreply.github.com>
2024-06-20 12:35:14 -0400

    Added `jq` to requirements, `pb` script to ./bin/

--- requirements.sh
+++ requirements.sh
@@ -6,5 +6,5 @@
 command -v npm >/dev/null 2>&1 && npm -g install readability-cli || echo "NPM is not installed. Use NVM to install NPM. Skipping."
 
 # Assuming Arch, may look into cross-distro solution later.
-sudo pacman -S newsboat dialog tree jpegoptim oxipng pkgfile bash-completion wl-clipboard whois fluidsynth soundfont-fluid
+sudo pacman -S newsboat dialog tree jpegoptim oxipng pkgfile bash-completion wl-clipboard whois fluidsynth soundfont-fluid jq
 
